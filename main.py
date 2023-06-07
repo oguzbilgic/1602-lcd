45c9b1f6cf049db5171490ea7664fef82696e45a
Oguz Bilgic <fisyonet@gmail.com>
2023-06-07 16:49:34 -0700

    Replace spaces

--- main.py
+++ main.py
@@ -5,27 +5,27 @@ from time import sleep
 class Port:
     def __init__(self, pin_nums, initial, led_pin = False):
         self.pins = list(map(lambda pin: Pin(pin, Pin.OUT, initial), pin_nums))
-        
+
         #if led_pin: self.led = Pin(led_pin, Pin.OUT)
-        
+
     def send(self, bits):
         for pos, pin in enumerate(reversed(self.pins)):
             bit = (bits >> pos) & 1
             pin.value(bit)
-            #if self.led : self.led.toggle() 
+            #if self.led : self.led.toggle()
             #print("write", pos, pin, "value", pin.value())
             #print(pinx.value())
         if False:
             timer = Timer()
             timer.init(freq=0.5, mode=Timer.ONE_SHOT, callback = self.blink)
-    
+
     def blink(self):
         self.led.toggle()
-            
+
     def set_in(self):
         for pos, pin in enumerate(self.pins):
             pin.mode(Pin.IN)
-        
+
     def read(self):
         for pos, pin in enumerate(self.pins):
             print("read", pos, pin, "value", pin.value())
@@ -38,15 +38,15 @@ class LCD:
     WRITE = 0b101
     BUSY = 0b110
     READ = 0b111
-    
+
     # Commands:
     CLEAR = 0x01
     HOME = 0x02
-    
+
     ENTRY = 0x04
     ENTRY_RIGHT = 0x02
     ENTRY_SIFHT = 0x01
-    
+
     DISPLAY = 0x08
     DISPLAY_ON = 0x04
     DISPLAY_CURSOR = 0x02
@@ -55,21 +55,21 @@ class LCD:
     SHIFT = 0x10
     SHIFT_DISPLAY = 0x08
     SHIFT_RIGHT = 0x04
-        
+
     FUNCTION = 0x20
     FUNCTION_8_BIT = 0x10
     FUNCTION_TWO_LINE = 0x08
     FUNCTION_FONT_TYPE = 0x04
 
     CGRAM_ADDRESS = 0x40
-    
+
     DDRAM_ADDRESS = 0x80
     DDRAM_ADDRESS_LINE_2 = 0x40
-    
+
     def __init__(self, mode_port, databus_port):
         self.mode_port = mode_port
         self.databus_port = databus_port
-        
+
         #self.pinrw = Pin(4, Pin.OUT)
         #self.pinrs = Pin(3, Pin.OUT)
         #self.pine = Pin(5, Pin.OUT)
@@ -79,33 +79,33 @@ class LCD:
         self.send_display(True, True, True)
         self.send_clear()
         self.send_entry(True, False)
-        
+
     # works
     def x__init__(self, mode_port, databus_port):
         self.mode_port = mode_port
         self.databus_port = databus_port
- 
-        
+
+
         sleep(0.2)
         #self.write_ddram(0x3c)
-        
+
         self.send_function(True, True, False)
         self.send_display(True, True, True)
-        
+
         self.send_clear()
         sleep(0.3)
         self.send_entry(True, False)
         sleep(0.5)
-                
+
         #self.display_string("hello/*&^%$#@")
-    
+
     # TODO: Implement
     def read(self):
         self.databus_port.set_in()
 
         self.mode_port.send(self.BUSY)
         self.databus_port.read()
-    
+
     # TODO
     def check_busy(self):
         self.pinrs.value(0)
@@ -116,30 +116,30 @@ class LCD:
             print("busy")
         print('done')
         return(self.pinb.value())
-    
-    
-    # TODO: Check busy            
+
+
+    # TODO: Check busy
     def send_instruction(self, instruction, mode = SEND, check_busy = True):
         #if check_busy: self.check_busy()
-        
+
         self.databus_port.send(instruction)
         self.mode_port.send(mode)
         sleep(0.001)
         self.mode_port.send(self.DONE)
         sleep(0.001)
-        
+
     def send_clear(self):
         self.send_instruction(self.CLEAR)
         sleep(0.01)
-            
+
     def send_home(self):
         self.send_instruction(self.HOME)
- 
+
     def send_entry(self, right = True, shift = True):
         instruction = self.ENTRY
         if right: instruction += self.ENTRY_RIGHT
         if shift: instruction += self.ENTRY_SIFHT
-        
+
         self.send_instruction(instruction)
 
     # Turns display, cursor and blinking on and off
@@ -149,37 +149,37 @@ class LCD:
         if on: instruction += self.DISPLAY_ON
         if on and cursor: instruction += self.DISPLAY_CURSOR
         if on and cursor and blink: instruction += self.DISPLAY_BLINK
-        
+
         self.send_instruction(instruction)
-    
+
     # Shifts the cursor or display
     def send_shift(self, display = True, right = True):
         instruction = self.SHIFT
         if display: instruction += self.SHIFT_DISPLAY
         if right: instruction += self.SHIFT_RIGHT
-        
+
         self.send_instruction(instruction)
-    
+
     # Sends a function instruction that is used for initizalizing the display
     def send_function(self, eight_bit = True, two_line = True, font_type = True):
         instruction = self.FUNCTION
         if eight_bit: instruction += self.FUNCTION_8_BIT
         if two_line: instruction += self.FUNCTION_TWO_LINE
         if font_type: instruction += self.FUNCTION_FONT_TYPE
-                
+
         self.send_instruction(instruction, self.SEND, False)
-    
+
     # Sets the cursor position
     def send_ddram_address(self, line = 1, column = 1):
         instruction = self.DDRAM_ADDRESS
         if line == 2 : instruction += self.DDRAM_ADDRESS_LINE_2
         instruction += column
-        
+
         self.send_instruction(instruction)
-        
+
     def write_ddram(self, data):
         self.send_instruction(data, self.WRITE)
-        
+
     def display_string(self, string):
         for char in string:
             self.write_ddram(ord(char))
